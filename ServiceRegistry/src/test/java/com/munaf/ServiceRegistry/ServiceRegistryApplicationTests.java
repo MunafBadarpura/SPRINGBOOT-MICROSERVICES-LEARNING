cf0116f7a02f8a899c0f8497ad6cdcb3aa4cc99f
MunafBadarpura <munafbadarpura11@gmail.com>
2025-02-12 15:45:43 +0530

    Eureka Server

---
+++ ServiceRegistry/src/test/java/com/munaf/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munaf.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

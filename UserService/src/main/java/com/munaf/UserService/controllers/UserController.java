36287f8f2eaf756fa1c355577b385d36cdee1b06
MunafBadarpura <munafbadarpura11@gmail.com>
2025-03-11 12:14:16 +0530

    Added Rate Limiting

--- UserService/src/main/java/com/munaf/UserService/controllers/UserController.java
+++ UserService/src/main/java/com/munaf/UserService/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.munaf.UserService.entities.User;
 import com.munaf.UserService.services.UserService;
 import com.munaf.UserService.utils.ResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,21 @@ public class UserController {
         return new ResponseEntity<>(responseModel, HttpStatus.CREATED);
     }
 
+    // FALLBACK METHOD FOR RATE-LIMITING: getAllUser
+    public ResponseEntity<ResponseModel> ratingHotelLimiterFallback(Long userId, Exception ex) {
+        ResponseModel responseModel = ResponseModel
+                .builder()
+                .data("SO MUCH REQUESTS, TRY AFTER SOME TIME")
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+                .build();
+
+        return new ResponseEntity<>(responseModel, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
     @GetMapping
+    @RateLimiter(name = "ratingHotelLimiter", fallbackMethod = "ratingHotelLimiterFallback")
     public ResponseEntity<ResponseModel> getAllUsers() {
         ResponseModel responseModel = ResponseModel
                 .builder()
@@ -40,7 +55,7 @@ public class UserController {
         return new ResponseEntity<>(responseModel, HttpStatus.OK);
     }
 
-    // FALLBACK METHOD FOR : getUserById
+    // FALLBACK METHOD FOR CircuitBreaker: getUserById
     public ResponseEntity<ResponseModel> ratingHotelFallback(Long userId, Exception ex) {
         ResponseModel responseModel = ResponseModel
                 .builder()
